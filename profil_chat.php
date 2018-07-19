8de7b51e21b0bbc90be9ca8c4ca72ea56ae0bc0a
Neths <vacher.michel@gmail.com>
2018-07-19 02:17:55 +0200

    profile chat dynamique

--- profil_chat.php
+++ profil_chat.php
@@ -11,6 +11,50 @@ session_start();
 
 	<?php include "nav.php"?>
 
+	<?php
+	require_once 'settings.php';
+	require_once 'connect.php';
+
+	$chat_selection = -1;
+
+	if(!empty($_GET['id_chat']))
+		$chat_selection = $_GET['id_chat'];
+
+	if (empty($_SESSION['id'])) {
+		echo 'Error!';
+	} else {
+		$chats = array();
+
+		$ssid = $_SESSION['id'];
+		$recup_infos = "SELECT * FROM chats WHERE id_utilisateur = '$ssid'";
+		$query = mysqli_query($connect, $recup_infos);
+		while($line = mysqli_fetch_assoc($query)) 
+		{
+			$id_chat = $line['id'];
+			$chat = array();
+
+			$chat["nom"] = $line["nom"];
+			
+			$query_attributs = mysqli_query($connect, "SELECT attributs.nom AS nom, attribut_chat.valeur AS valeur FROM attribut_chat INNER JOIN attributs ON attributs.id = attribut_chat.id_attribut WHERE attribut_chat.id_chat = '$id_chat'");
+
+			$attributs = array();
+			while($attr = mysqli_fetch_assoc($query_attributs)) {
+				$attributs[$attr['nom']] = $attr['valeur'];
+			}
+			$chat['attributs'] = $attributs;
+
+			$query_maladies = mysqli_query($connect, "SELECT pathologie.nom AS nom FROM chat_pathologie INNER JOIN pathologie ON pathologie.id = chat_pathologie.id_pathologie WHERE chat_pathologie.id_chat = '$id_chat'");
+
+			$maladies = array();
+			while($maladie = mysqli_fetch_assoc($query_maladies)) {
+				$maladies[] = $maladie['nom'];
+			}
+			$chat['maladies'] = $maladies;
+
+			$chats[$id_chat] = $chat;
+		}
+	} ?>
+
 	<div class="row">
 		<div class="col s12 m3">
 			<div class="card card-grey">
@@ -49,48 +93,71 @@ session_start();
 					</div>
 					<div class="col s12">
 						<ul class="tabs tab-chats">
-							<li class="tab col s3"><a class="active" href="#chat-1">Chat 1</a></li>
-							<li class="tab col s3"><a href="#chat-2">Chat 2</a></li>
-							<li class="tab col s3"><a href="#chat-3">Chat 3</a></li>
+							<?php 
+							foreach ($chats as $key => $chat) {
+								if($key == $chat_selection) {
+									echo "<li class=\"tab col s3\"><a class=\"active\" href=\"#chat-".$key."\">".$chat['nom']."</a></li>";
+								}
+								else {
+									echo "<li class=\"tab col s3\"><a href=\"#chat-".$key."\">".$chat['nom']."</a></li>";
+								}
+							}
+							?>
 						</ul>
+					
 
-
-						<div class="grey lighten-4">
-							<div class="row">
-								<div class="col s12">
-									<div class="section color-vert">
-										<h5>Section 1</h5>
-										<p>Stuff</p>
-									</div>
-									<div class="divider"></div>
-									<div class="section color-vert">
-										<h5>Section 2</h5>
-										<p>Stuff</p>
-									</div>
-									<div class="divider"></div>
-									<div class="section color-vert">
-										<h5>Section 3</h5>
-										<p>Stuff</p>
-									</div>
-									<div class="divider"></div>
-									<div class="section color-rouge">
-										<div class="card" id="popup" style="margin:auto">
-											<ul class="collapsible">
-												<li>
-													<div class="collapsible-header"><i class="material-icons">filter_drama</i>NOTIFICATIONS</div>
-													<div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-												</li>
+						<?php 
+							foreach ($chats as $key => $chat) { ?>
+								<div id="chat-<?php echo $key ?>" class="col s12 grey lighten-4">
+								<div class="row">
+									<div class="col s12">
+										<div class="section color-vert">
+											<h5>Attributs</h5>
+											<ul>
+												<?php 
+													foreach ($chat['attributs'] as $nom => $valeur) {
+														echo "<li>$nom : $valeur</li>";
+													}
+												?>
 											</ul>
+										</div>
+										<div class="divider"></div>
+										<div class="section color-vert">
+											<h5>Maladies</h5>
+											<ul>
+												<?php 
+													foreach ($chat['maladies'] as $nom) {
+														echo "<li>$nom</li>";
+													}
+												?>
+											</ul>
+										</div>
+										<div class="divider"></div>
+										<div class="section color-rouge">
+											<div class="card" id="popup" style="margin:auto">
+												<ul class="collapsible">
+													<li>
+														<div class="collapsible-header"><i class="material-icons">filter_drama</i>NOTIFICATIONS</div>
+														<div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+													</li>
+												</ul>
+											</div>
 										</div>
 									</div>
 								</div>
 							</div>
-						</div>
+							<?php } ?>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
+
+	<script>
+		$(document).ready(function(){
+			$('.tabs').tabs();
+		});
+	</script>
 
 </body>
 
